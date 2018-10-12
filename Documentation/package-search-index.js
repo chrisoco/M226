d67d9a74f760bdfb73cefc91ea0efbfb408d3ae6
Chris <christopher.oconnor@santismail.ch>
2018-10-12 15:49:46 +0200

    Revert "Generated JavaDoc"
    This reverts commit b9d599d839dcccbfadbf2f400dea889ceac7d9df.

--- Documentation/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"application"},{"l":"application.view"},{"l":"application.view.tab.book"},{"l":"application.view.tab.home"},{"l":"application.view.tab.person"},{"l":"application.view.tab.rent"}]
